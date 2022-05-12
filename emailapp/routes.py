0062d3bbb4d5d95c1c1033e4470a98a6013c642f
rasmus <ahtavarasmus@gmail.com>
2022-05-12 20:57:24 +0300

    fixing stuff

--- emailapp/routes.py
+++ emailapp/routes.py
@@ -271,7 +271,71 @@ def home():
     DaysLeftToFill = 42 - days_in_month - len(fill_last_month)
     fill_next_month = [item for item in range(1, DaysLeftToFill + 1)] 
 
-    curr_categories, user_trainings=user_trainings, current_week=curr_week, existing_weeks=existing_weeks)
+    
+    # finding right month and year. also cats and ints
+    tcats = []
+    raw_tints = []
+    for mon in current_user.months:
+        if mon.year.num == current_year and mon.num == current_month_int:
+            tcats = (mon.training_categories)
+            raw_tints = (mon.training_intensities)
+    # removing duplicates
+    tints = list(dict.fromkeys(raw_tints))
+    # Category - Time - Chart
+    c_times = []
+    for tc in tcats:
+        time_tracker = 0
+        for i in tc.intensities:
+            time_tracker += int(i.overall_time)
+        c_times.append(time_tracker)
+    tc_times = (c_times)
+    # Intensity - Time - Chart
+    i_times = []
+
+    for i in tints:
+        i_times.append(int(i.overall_time))
+    ti_times = ( i_times )
+
+    colorpalette = ["#003f5c","#2f4b7c","#665191","#a05195","#d45087","#f95d6a","#ff7c43","#ffa600"]
+    cat_colors = ( colorpalette[:len(tcats)] )
+    int_colors = ( colorpalette[:len(tints)] )
+
+
+
+
+    if request.method == 'POST':
+        year = request.form['year']
+        month = datetime.datetime.strptime(request.form['month'], "%B").month
+        # adding to the session
+        session['year'] = year
+        session['month'] = month
+
+
+        # i have to put the current year and month to database always 
+        # temporally cause that way i can refresh the page for 
+        # next year and remember the selection for that users session.
+        return redirect(url_for('routes.home'))
+
+
+    
+    return render_template('home.html',
+    user=current_user,
+    active_tag='active',
+    current_day=current_day,
+    current_month_int=current_month_int,
+    current_month_name=current_month_name,
+    current_year=current_year,
+    days_in_month=days_in_month,
+    list_of_days=list_of_days,
+    fill_last_month=fill_last_month,
+    fill_next_month=fill_next_month,
+    cat_set=zip(tc_times,tcats,cat_colors),
+    tc_times=tc_times,
+    tcats=tcats,
+    cat_colors=cat_colors,
+    max=17000,
+    int_set=zip(ti_times,tints,int_colors)
+    )
 
 @login_required
 @routes.route("/training-<timeofday>-<day>-<month>-<year>/", methods=['POST', 'GET'])
@@ -310,6 +374,122 @@ def training_day(timeofday, day, month, year):
     if request.method == 'POST':
         raw_main = request.form['main']
         add_training_to_db(raw_main,timeofday,day,month,year)
+        return redirect(URL)
+        # making a datetime object for this day
+        # training_date = datetime.date(int(year),int(month),int(day))
+        # making the training object
+        new_training = Training(user=current_user, name=raw_main, timeofday=timeofday,training_date=training_date)
+        try:
+            db.session.add(new_training)
+            db.session.commit()
+        except:
+            return 'There was an arror adding training to db'
+
+        # getting existing category objects
+        ecategories = current_user.categories
+        # if no, redirect to making some categories first
+        if ecategories == []:
+            return redirect(url_for('routes.settings'))
+        
+        # making a list of category names
+        cat_names = []
+        for cat in ecategories:
+            cat_names.append(cat.name)
+    
+        # checking which section had the most time so i can name the whole training by it
+        leading_sec = 0
+        # splitting training into parts
+        sections = raw_main.split('+')
+        for section in sections:
+            ttype, intensity, ttime = section.split(',')
+            section_obj = TrainingSection(user=current_user,name=ttype)
+
+            if int(ttime) > leading_sec:
+                new_training.name = ttype
+                leading_sec = int(ttime) 
+            # finding the existing category for ttype
+            if not ttype in cat_names:
+                return 'the category:', ttype, 'not found, create it in settings:)'
+            
+            # assigning category
+            for item in ecategories:
+                if ttype == item.name:
+                    section_obj.category = item
+                    break
+
+            # assigning intensity
+
+            # assigning intensity
+            section_obj.intensity = intensity
+            section_obj.time = ttime
+            section_obj.training = new_training
+
+            # commiting section_obj
+            db.session.add(section_obj)
+            db.session.commit()
+            # checking if there's an existing year object
+            year_found = False
+            for item in current_user.years:
+                if item.num == int(year):
+                    year_obj = item
+                    year_found = True
+            if not year_found:
+                year_obj = Year(user=current_user, num=int(year))
+
+            db.session.add(year_obj)
+            db.session.commit()
+
+            # checking if there's an existing month object
+            month_found = False
+            for item in current_user.months:
+                if item.num == int(month):
+                    month_obj = item
+                    month_found = True
+            if not month_found:
+                month_obj = Month(user=current_user, num=int(month), name=date_obj.strftime("%B"), year=year_obj)
+
+
+            db.session.add(month_obj)
+            db.session.commit()
+            
+            # adding everything also to Month objects summary
+            mtc_found = False
+            for c in month_obj.training_categories:
+                if c.name == ttype:
+                    mtc = c
+                    mtc_found = True
+            if not mtc_found:
+                mtc = MonthsTrainingCategory(name=ttype,month=month_obj)
+                db.session.add(mtc)
+                db.session.commit()
+
+            # searching for existing mci
+            mci_found = False
+            for i in mtc.intensities:
+                if i.name == intensity:
+                    mci = i
+                    mci_found = True
+            if not mci_found:
+                mci = MonthsCategorysIntensity(name=intensity, month=month_obj)
+                db.session.add(mci)
+                db.session.commit()
+
+
+    
+            if mci.overall_time != None:
+                etime = int(mci.overall_time)
+            else:
+                etime = 0
+
+            etime += int(ttime)
+            mci.overall_time = str(etime)
+            mci.months_training_category = mtc
+            db.session.add(mci)
+            db.session.commit()
+
+
+        db.session.add(new_training)
+        db.session.commit()
         return redirect(URL)
     return render_template('training_day.html', 
     user=current_user, 
